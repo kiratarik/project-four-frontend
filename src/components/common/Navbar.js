8ef1d884255eeb1ac21be090366c2b657ad47bbb
Tarik Canessa-Wright <tarik.canessa.wright@gmail.com>
2021-09-12 18:31:44 +0100

    navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
+
+  const userId = 1
+
   return (
     <>
       <h1>NavBar</h1>
+      <Link to="/">Home</Link>
+      <Link to="/games">Games</Link>
+      <Link to="/users">Users</Link>
+      <Link to={`/users/${userId}`}>Home</Link>
+      <Link to="/login">Home</Link>
+      <Link to="/register">Home</Link>
     </>
   )
 }
